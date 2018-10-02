b04da448b53a2502d342e166c161c87ea2e39da0
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2018-10-02 23:11:17 +0100

    initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+      inputLength: 0,
+      inputValue: ''
+  };
+
+  setParagraphLength = (event) => {
+    const state = {...this.state}
+
+  };
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <input type="text" onChange={(event) => this.setParagraphLength (event)} value={this.state.inputValue}/>
+          <p>Input length is: {this.state.inputLength}</p>
       </div>
     );
   }
