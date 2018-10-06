6e7d856501df20e62a7c5feb023bdc5046646247
Tomasz-Kluczkowski <tomaszk1@hotmail.com.uk>
2018-10-06 11:14:54 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
 
-  state = {
-      inputLength: 0,
-      inputValue: ''
-  };
+    state = {
+        inputs: {
+            inputLength: 0,
+            inputValue: ''
+        }
+    };
 
-  setParagraphLength = (event) => {
-    const state = {...this.state}
-    console.log(event.target.value)
+    setParagraphLength = (event) => {
+        const inputs = {...this.state.inputs};
+        console.log(inputs);
+        console.log(event.target.value);
+        inputs.inputValue = event.target.value;
+        inputs.inputLength = inputs.inputValue.length;
+        this.setState(
+            {inputs: inputs}
+        )
+    };
 
-  };
 
-
-  render() {
-    return (
-      <div className="App">
-          <input type="text" onChange={(event) => this.setParagraphLength (event)} value={this.state.inputValue}/>
-          <p>Input length is: {this.state.inputLength}</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <input type="text"
+                       onChange={(event) => this.setParagraphLength(event)}
+                       value={this.state.inputs.inputValue}/>
+                <p>Input length is: {this.state.inputs.inputLength}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
